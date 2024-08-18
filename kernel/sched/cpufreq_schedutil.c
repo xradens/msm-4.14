c58b26cfc79d49fac45caf5991a057142a5a6972
Richard Raya <rdxzv.dev@gmail.com>
2024-08-18 03:51:39 -0300

    schedutil: Bump down rate limits to 20ms
    Change-Id: I69afb0685a0748b57d8e0da948156e708217a691
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -1005,7 +1005,7 @@ static int sugov_init(struct cpufreq_policy *policy)
 	}
 
 	tunables->up_rate_limit_us = 500;
-	tunables->down_rate_limit_us = 1000;
+	tunables->down_rate_limit_us = 20000;
 	tunables->hispeed_load = DEFAULT_HISPEED_LOAD;
 	tunables->hispeed_freq = 0;
 
